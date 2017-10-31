5c5b24e4362ac3fcae3ab0b04293b5170b19f1ef
Li SH <li012589li@outlook.com>
2017-10-31 22:55:36 +0800

    revert tested

--- model/realnvp_.py
+++ model/realnvp_.py
@@ -1,18 +1,61 @@
-from model import RealNVPtemplate,LayerTemplate
+if __name__ == "__main__":
+    import os
+    import sys
+    sys.path.append(os.getcwd())
 
-class VanillaRealNVP(RealNVPtemplate):
-    def __init__(self,numVars,layers,name = "vanillaRealNVP"):
-        super(RealNVP,self).__init__(layers,name)
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+from model import RealNVPtemplate,PriorTemplate
+
+class Gaussian(PriorTemplate):
+    def __init__(self,numVars,name = "gaussian"):
+        super(Gaussian,self).__init__(name)
         self.numVars = numVars
-        self.halfNumVars = int(numVars/2)
-        maskOne = torch.ones(self.halfNumVars)
-        maskZero = torch.zeros(self.halfNumVars)
-        self.mask = torch.cat(tmp,tmp_,0)
+    def __call__(self,batchSize):
+        return Variable(torch.randn(batchSize,self.numVars))
+    def logProbability(self,z):
+        return -0.5*(z**2)
 
-class CouplingLayer(LayerTemplate):
-    def __init__(self,s,t,name="CouplingLayer"):
-        super(CouplingLayer,self).__init__(s,t,name)
+class Mlp(nn.Module):
+    def __init__(self,inNum,outNum,hideNum,name="mlp"):
+        super(Mlp, self).__init__()
+        self.fc1 = nn.Linear(inNum,hideNum)
+        self.fc2 = nn.Linear(hideNum,outNum)
+        self.name = name
+    def forward(self,x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
 
+class RealNVP(RealNVPtemplate):
+    def __init__(self,sList,tList,prior):
+        super(RealNVP,self).__init__(sList,tList,prior)
 
 if __name__ == "__main__":
-    model = VanillaRealNVP()
+
+    gaussian = Gaussian(2)
+
+    sList = [Mlp(1,1,10),Mlp(1,1,10),Mlp(1,1,10),Mlp(1,1,10)]
+    tList = [Mlp(1,1,10),Mlp(1,1,10),Mlp(1,1,10),Mlp(1,1,10)]
+
+    realNVP = RealNVP(sList,tList,gaussian)
+
+    mask = torch.ByteTensor([0,1])
+    x = gaussian(10)
+    print("original")
+    print(x)
+
+    z,_ = realNVP.encode(x,mask)
+
+    print("Forward")
+    print(z)
+
+    zp,_ = realNVP.decode(z,mask)
+
+    print("Backward")
+    print(zp)
+
