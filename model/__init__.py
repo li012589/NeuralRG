9ad1e73dddb8c333c1593a6304f55e629fa11586
Li SH <li012589li@outlook.com>
2017-11-01 17:25:54 +0800

    expose realnvp to outside

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,4 @@
 from template import RealNVPtemplate,PriorTemplate
+from realnvp import Gaussian,MLP,RealNVP
 
-__all__ = ['RealNVPtemplate','PriorTemplate']
+__all__ = ['RealNVPtemplate','PriorTemplate','Gaussian','MLP','RealNVP']
