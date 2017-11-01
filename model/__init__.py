4410d91d8e85aacecdea796e8fd0e17164ab89a8
Li SH <li012589li@outlook.com>
2017-11-01 23:17:06 +0800

    autopeped __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,5 @@
-from model.template import RealNVPtemplate,PriorTemplate
-from model.realnvp import Gaussian,MLP,RealNVP,CNN
+from model.template import RealNVPtemplate, PriorTemplate
+from model.realnvp import Gaussian, MLP, RealNVP, CNN
 
-__all__ = ['RealNVPtemplate','PriorTemplate','Gaussian','MLP','RealNVP','CNN']
+__all__ = ['RealNVPtemplate', 'PriorTemplate',
+           'Gaussian', 'MLP', 'RealNVP', 'CNN']
