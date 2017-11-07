1c87746271ecd2fa9a82498086d01631782df3c3
Li SH <li012589li@outlook.com>
2017-11-07 22:53:54 +0800

    add new figures

--- train/learn_model.py
+++ train/learn_model.py
@@ -12,22 +12,18 @@ from model import Gaussian,MLP,RealNVP
 from train.objectives import Ring2D, Ring5 
 
 def fit(Nlayers, Hs, Ht, Nepochs, supervised,ifCuda = False):
+    LOSS=[]
     xy = np.loadtxt('train.dat', dtype=np.float32)
     x_data = Variable(torch.from_numpy(xy[:, 0:-1]))
     if ifCuda:
         x_data = x_data.cuda()
-    print (x_data.data.shape)
 
     if supervised:
         y_data = Variable(torch.from_numpy(xy[:, -1]))
         if ifCuda:
             y_data = y_data.cuda()
-        print (y_data.data.shape)
-
-    print (x_data.data.shape)
 
     Nvars = x_data.data.shape[-1]
-    print (Nvars)
 
     gaussian = Gaussian([Nvars])
 
@@ -51,6 +47,7 @@ def fit(Nlayers, Hs, Ht, Nepochs, supervised,ifCuda = False):
             loss = -logp.mean() 
 
         print (epoch, loss.data[0])
+        LOSS.append(loss.data[0])
 
         optimizer.zero_grad()
         loss.backward() 
@@ -58,9 +55,9 @@ def fit(Nlayers, Hs, Ht, Nepochs, supervised,ifCuda = False):
 
     saveDict = model.saveModel({})
     torch.save(saveDict, './'+model.name)
-    return Nvars, x_data, model
+    return Nvars, x_data, model, LOSS
 
-def visualize(Nvars, x_data, model, target):
+def visualize(Nvars, x_data, model, target,Loss,supervised):
 
     #after training, generate some data from the network
     Ntest = 1000 # test samples
@@ -75,27 +72,46 @@ def visualize(Nvars, x_data, model, target):
     logp_model_test = model.logProbability(x)
     logp_data_test = target(x)
 
-    plt.figure() 
-    plt.scatter(logp_model_train.data.numpy(), logp_data_train.data.numpy(), alpha=0.5, label='train')
-    plt.scatter(logp_model_test.data.numpy(), logp_data_test.data.numpy(), alpha=0.5, label='test')
+    plt.figure()
+    plt.scatter(logp_model_train.data.numpy(), logp_data_train.data.numpy(), alpha=0.5, label='training samples')
+    plt.scatter(logp_model_test.data.numpy(), logp_data_test.data.numpy(), alpha=0.5, label='generated samples')
+    plt.xlabel('$\log{P(model)}$')
+    plt.ylabel('$\log{P(baseline)}$')
+    plt.legend()
+    plt.title("$\log{P(x)}$")
 
-    plt.xlabel('model')
-    plt.ylabel('baseline')
-    plt.legend() 
-   
     x_data = x_data.data.numpy()
     x = x.data.numpy()
 
     plt.figure()
-    plt.scatter(x_data[:,0], x_data[:,1], alpha=0.5, label='original')
-    plt.scatter(x[:,0], x[:,1], alpha=0.5, label='generated')
-
+    plt.scatter(x_data[:,0], x_data[:,1], alpha=0.5)
     plt.xlim([-5, 5])
     plt.ylim([-5, 5])
-
     plt.xlabel('$x_1$')
     plt.ylabel('$x_2$')
-    plt.legend() 
+    plt.title("Training samples")
+
+    plt.figure()
+    plt.scatter(x_data[:,0], x_data[:,1], alpha=0.5, label='training samples')
+    plt.scatter(x[:,0], x[:,1], alpha=0.5, label='generated samples')
+    plt.xlim([-5, 5])
+    plt.ylim([-5, 5])
+    plt.xlabel('$x_1$')
+    plt.ylabel('$x_2$')
+    if supervised:
+        plt.title("Supervised")
+    else:
+        plt.title("Unsupervised")
+    plt.legend()
+
+    plt.figure()
+    plt.plot(LOSS)
+    plt.xlabel('iterations')
+    plt.ylabel('loss')
+    if supervised:
+        plt.title("MSE loss")
+    else:
+        plt.title("NLL loss")
 
     plt.show()
 
@@ -122,12 +138,12 @@ if __name__=="__main__":
         print ('what target ?', args.target)
         sys.exit(1)
 
-    Nvars, x_data, model = fit(args.Nlayers, 
+    Nvars, x_data, model, LOSS= fit(args.Nlayers, 
                                args.Hs, 
                                args.Ht, 
                                args.Nepochs, 
                                args.supervised,
                                args.cuda)
 
-    visualize(Nvars, x_data, model, target) 
+    visualize(Nvars, x_data, model, target,LOSS,args.supervised) 
 
