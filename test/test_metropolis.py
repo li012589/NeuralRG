938f465b71e4e75a15162eea2ef05ac3dccafae7
Li SH <li012589li@outlook.com>
2017-11-10 11:58:35 +0800

    add test for metroplois

---
+++ test/test_metropolis.py
@@ -0,0 +1,28 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+torch.manual_seed(42)
+
+import numpy as np
+from numpy.testing import assert_array_almost_equal,assert_array_equal
+from train.metropolis import MCMC
+from train.objectives import Ring2D, Ring5, Wave
+
+from subprocess import Popen, PIPE
+import pytest
+
+noCuda = 0
+try:
+    p  = Popen(["nvidia-smi","--query-gpu=index,utilization.gpu,memory.total,memory.used,memory.free,driver_version,name,gpu_serial,display_active,display_mode", "--format=csv,noheader,nounits"], stdout=PIPE)
+except OSError:
+    noCuda = 1
+
+skipIfNoCuda = pytest.mark.skipif(noCuda == 1,reason = "NO cuda insatllation, found through nvidia-smi")
+
+def test_metropolis():
+    pass
