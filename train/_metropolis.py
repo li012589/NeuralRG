42454e24e195237a526d0f10e888735323ac36eb
Li SH <li012589li@outlook.com>
2017-11-10 16:13:41 +0800

    add comments

--- train/_metropolis.py
+++ train/_metropolis.py
@@ -21,10 +21,19 @@ class MCMC:
         nvars (int): number of variables.
         batchsize (int): batch size.
         target (Target): target log-probability.
-        model (): model used for update proposal.
+        prior (nn.Module): sampler.
+        collectdata (bool): if to collect all data generated.
     """
 
-    def __init__(self,  batchsize, target, prior, collectdata):
+    def __init__(self, batchsize, target, prior, collectdata):
+        """
+        Init MCMC class
+        Args:
+            batchSize (int): batch size.
+            target (Target): target log-probability.
+            prior (sampler): sampler.
+            collectdata (bool): if to collect all data generated.
+        """
         self.batchsize = batchsize
         self.target = target
         self.prior = prior
@@ -38,10 +47,25 @@ class MCMC:
 
     @staticmethod
     def _accept(e1,e2):
+        """
+        This method gives if or not update x.
+        Args:
+            e1 (torch.Tensor): energy of original x.
+            e2 (torch.Tensor): energy of proposed x.
+        Return:
+            ifUpdate (bool): if update x.
+        """
         diff = e1-e2
         return diff.exp()-diff.uniform_()>=0.0
 
     def run(self,ntherm,nmeasure,nskip):
+        """
+        This method start sampling.
+        Args:
+            ntherm (int): number of steps used in thermalize.
+            nmeasure (int): number of steps used in measure.
+            nskip (int): number of steps skiped in measure.
+        """
         self.nmeasure= nmeasure
         self.ntherm = ntherm
 
@@ -55,11 +79,13 @@ class MCMC:
             self.measure()
         self.accratio /= float(nmeasure*nskip)
 
-        print ('#accratio:', self.accratio)
+        #print ('#accratio:', self.accratio)
 
     def step(self):
-        #sample prior
-        x = self.prior(self.batchsize)                # pass prior directly to the output
+        """
+        This method run a step of sampling.
+        """
+        x = self.prior(self.batchsize)
         accept = self._accept(
             self.target(x.data)-self.prior.logProbability(x).data,
             self.target(self.x)-self.prior.logProbability(Variable(self.x,volatile=True)).data)
@@ -72,15 +98,22 @@ class MCMC:
 
         return accratio
 
-    def measure(self):
-
+    def measure(self,measureFn = None):
+        """
+        This method measures some varibales.
+        Args:
+            measureFn (function): function to measure variables. If None, will run measure at target class.
+        """
         if self.collectdata:
             x = self.x.numpy()
             logp = self.target(self.x).numpy()
             logp.shape = (-1, 1)
             self.data.append(np.concatenate((x, logp), axis=1))
 
-        self.measurements.append( self.target.measure(self.x) )
+        if measureFn is None:
+            self.measurements.append( self.target.measure(self.x) )
+        else:
+            self.measurements.append( measureFn(self.x) )
 
 if __name__ == '__main__':
     import os, sys
