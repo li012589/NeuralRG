f029073f7d8a7c517f2cd7e817b6114d8d0e1a97
Li SH <li012589li@outlook.com>
2017-11-14 15:08:03 +0800

    add test

---
+++ test/test_parallel.py
@@ -0,0 +1,50 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+torch.manual_seed(42)
+
+import numpy as np
+from numpy.testing import assert_array_almost_equal,assert_array_equal
+from model import Gaussian,MLP,RealNVP,CNN
+
+from subprocess import Popen, PIPE
+import pytest
+
+noCuda = 0
+try:
+    p  = Popen(["nvidia-smi","--query-gpu=index,utilization.gpu,memory.total,memory.used,memory.free,driver_version,name,gpu_serial,display_active,display_mode", "--format=csv,noheader,nounits"], stdout=PIPE)
+except OSError:
+    noCuda = 1
+
+def test_parallel():
+    gaussian3d = Gaussian([2,4,4])
+    x3d = gaussian3d(3)
+    #z3dp = z3d[:,0,:,:].view(10,-1,4,4)
+    #print(z3dp)
+
+    netStructure = [[3,2,1,1],[4,2,1,1],[3,2,1,0],[1,2,1,0]] # [channel, filter_size, stride, padding]
+
+    sList3d = [CNN([2,4,2],netStructure),CNN([2,4,2],netStructure),CNN([2,4,2],netStructure),CNN([2,4,2],netStructure)]
+    tList3d = [CNN([2,4,2],netStructure),CNN([2,4,2],netStructure),CNN([2,4,2],netStructure),CNN([2,4,2],netStructure)]
+
+    realNVP3d = RealNVP([2,4,4], sList3d, tList3d, gaussian3d)
+    #mask3d = realNVP3d.createMask("checkerboard")
+
+    z3d = realNVP3d.generate(x3d,2)
+    print("3d forward:")
+    #print(z3d)
+
+    zp3d = realNVP3d.inference(z3d,2)
+    print("Backward")
+    #print(zp3d)
+
+    print("3d logProbability")
+    print(realNVP3d.logProbability(z3d,2))
+
+if __name__ == "__main__":
+    test_parallel()
