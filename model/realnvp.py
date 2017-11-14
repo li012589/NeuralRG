22cbb5a37328d4861611abbfaa5ec6550dd49a35
Li SH <li012589li@outlook.com>
2017-11-14 23:42:18 +0800

    debug

--- model/realnvp.py
+++ model/realnvp.py
@@ -183,11 +183,9 @@ class RealNVP(RealNVPtemplate):
         self.createMask(maskType)
 
     def __copy__(self):
-        tcpyList = copy.deepcopy(self.tList)
-        scpyList = copy.deepcopy(self.sList)
-        cpy = RealNVP(self.shapeList,[],[],self.prior,self.maskType,self.name)
-        cpy.tList = tcpyList
-        cpy.sList = scpyList
+        tcpyList = [copy.deepcopy(t) for t in self.tList]
+        scpyList = [copy.deepcopy(s) for s in self.sList]
+        cpy = RealNVP(self.shapeList,scpyList,tcpyList,self.prior,self.maskType,self.name)
         return cpy
 
     def createMask(self, maskType="channel", ifByte=1):
