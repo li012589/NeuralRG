b71c40afa05eee8c1ed7e6d232d75ff3b9fd29fe
Li SH <li012589li@outlook.com>
2017-11-16 21:07:06 +0800

    remove parallel from __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,5 @@
 from .template import RealNVPtemplate, PriorTemplate
 from .realnvp import Gaussian, MLP, RealNVP, CNN
-from .parallel import parallelize
 
 __all__ = ['RealNVPtemplate', 'PriorTemplate',
-           'Gaussian', 'MLP', 'RealNVP', 'CNN','parallelize']
+           'Gaussian', 'MLP', 'RealNVP', 'CNN']
