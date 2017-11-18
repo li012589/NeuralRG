3ffdce9f4086edc1957e95f6a7f760c4b07d53c6
Li SH <li012589li@outlook.com>
2017-11-19 03:03:06 +0800

    hmc has same apis as metropolis

--- train/hmc.py
+++ train/hmc.py
@@ -7,18 +7,20 @@ import torch
 from torch.autograd import Variable
 from utils.autoCorrelation import autoCorrelationTimewithErr
 from utils.acceptRate import acceptanceRate
+from numpy.testing import assert_array_almost_equal,assert_array_equal
 
 def metropolis(e1,e2):
     diff = e1-e2
     return diff.exp()-diff.uniform_()>=0.0
 
 class HMCSampler:
-    def __init__(self,model,prior,stepSize=0.1,interSteps=10,targetAcceptRate=0.65,stepSizeMin=0.001,stepSizeMax=1000,stepSizeChangeRatio=0.03,dynamicStepSize=False):
+    def __init__(self,model,prior,collectdata,stepSize=0.1,interSteps=10,targetAcceptRate=0.65,stepSizeMin=0.001,stepSizeMax=1000,stepSizeChangeRatio=0.03,dynamicStepSize=False):
         pass
         self.model = model
         self.prior = prior
         self.stepSize = stepSize
         self.interSteps = interSteps
+        self.collectdata = collectdata
         self.dynamicStepSize = dynamicStepSize
         if self.dynamicStepSize:
             self.targetAcceptRate = targetAcceptRate
@@ -53,21 +55,58 @@ class HMCSampler:
     def hamiltonian(energy,v):
         return energy+0.5*torch.sum(v**2,1)
 
-    def run(self,steps,batchSize):
+    def step(self,z):
+        v = torch.randn(z.size())
+        x = v.clone()
+        zp,vp = self.hmcUpdate(z,v,self.model,self.stepSize,self.interSteps)
+        accept = metropolis(self.hamiltonian(self.model(z),v),self.hamiltonian(self.model(zp),vp))
+        if self.dynamicStepSize:
+            self.stepSize = self.updateStepSize(accept,self.stepSize)
+        #print(type(accept.numpy()))
+        #accept = np.array([accept.numpy()]*self.model.nvars).transpose()
+        #mask = 1-accept
+        #x = torch.from_numpy(z.numpy()*mask +zp.numpy()*accept)
+        accratio = accept.float().mean()
+        accept = accept.view(-1,1)
+        x.masked_scatter_(accept, torch.masked_select(z, accept))
+        #assert_array_almost_equal(x.cpu().numpy(),z.cpu().numpy())
+        return accratio,x
+
+    def run(self,batchSize,ntherm,nmeasure,nskip):
         z = self.prior(batchSize)
+        for _ in range(ntherm):
+            _,z = self.step(z)
+
         zpack = []
-        for i in range(steps):
-            v = torch.randn(z.size())
-            zp,vp = self.hmcUpdate(z,v,self.model,self.stepSize,self.interSteps)
-            accept = metropolis(self.hamiltonian(self.model(z),v),self.hamiltonian(self.model(zp),vp))
-            if self.dynamicStepSize:
-                self.stepSize = self.updateStepSize(accept,self.stepSize)
-            #print(type(accept.numpy()))
-            accept = np.array([accept.numpy()]*self.model.nvars).transpose()
-            mask = 1-accept
-            z = torch.from_numpy(z.numpy()*mask +zp.numpy()*accept)
-            zpack.append(z.numpy())
-        return zpack
+        measurePack = []
+        accratio = 0.0
+        for _ in range(nmeasure):
+            for _ in range(nskip):
+                a,z = self.step(z)
+                accratio += a
+            if self.collectdata:
+                z_ = z.cpu().numpy()
+                logp = self.model(z).cpu().numpy()
+                logp.shape = (-1, 1)
+                zpack.append(np.concatenate((z_, logp), axis=1))
+                #zpack.append(z_)
+            measure = self.measure(z)
+            measurePack.append(measure)
+        accratio /= float(nmeasure * nskip)
+        print ('#accratio:', accratio)
+        return zpack,measurePack,accratio
+
+    def measure(self, x,measureFn=None):
+        """
+        This method measures some varibales.
+        Args:
+            measureFn (function): function to measure variables. If None, will run measure at target class.
+        """
+        if measureFn is None:
+            measurements = self.model.measure(x)
+        else:
+            measurements = measureFn(x)
+        return measurements
 
 if __name__ == "__main__":
     import os
@@ -82,18 +121,8 @@ if __name__ == "__main__":
         return torch.randn(batchSize,modelSize)
 
     model = Phi4(3, 2, 1.0, 1.0)
-    sampler = HMCSampler(model,prior,dynamicStepSize=True)
+    sampler = HMCSampler(model,prior,True,dynamicStepSize=True)
     BatchSize = 100
-    Steps = 800
-    BurnIn = 300
-    bins = 2
-    res = sampler.run(Steps,BatchSize)
-    res=np.array(res)
-    z_o = res[BurnIn:,:]
-    z_ = np.reshape(z_o,[-1,modelSize])
-    z1_,z2_= z_[:,0],z_[:,1]
-    print("mean: ",np.mean(z1_))
-    print("std: ",np.std(z1_))
-    autoCorrelation,error =  autoCorrelationTimewithErr(z_o[:,:,0],bins)
-    acceptRate = acceptanceRate(z_o)
-    print('Acceptance Rate:',(acceptRate),'Autocorrelation Time:',(autoCorrelation))
+    res,mres,_ = sampler.run(BatchSize,30,10,5)
+    #print((res))
+    #print(len(res))
