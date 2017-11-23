e91f0e6d3e9feeb3b0d746d880ca09de18c206e2
Li SH <li012589li@outlook.com>
2017-11-23 12:11:41 +0800

    add frameworks

--- bootstrap.py
+++ bootstrap.py
@@ -7,7 +7,24 @@ from torch.autograd import Variable
 import numpy as np
 
 from model import Gaussian,MLP,RealNVP
-from train import Ring2D, Ring5, Wave, Phi4, MCMC, fit
+from train import Ring2D, Ring5, Wave, Phi4, MCMC, HMCSampler, fit
 
+class buffer(object):
+    def __init__(maximum):
+        pass
+    def draw(batchSize):
+        pass
+    def push(data):
+        pass
+    def _kill(batchSize):
+        pass
 
+def boot():
+    pass
+
+def strap():
+    pass
+
+def main():
+    pass
 
