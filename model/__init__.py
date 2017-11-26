c490f5fa916326f13a72e94c5bcbe2da082516ea
Li SH <li012589li@outlook.com>
2017-11-26 16:35:24 +0800

    debug

--- model/__init__.py
+++ model/__init__.py
@@ -3,4 +3,4 @@ from .realnvp import Gaussian, RealNVP
 from .layers import MLP, CNN, FC
 
 __all__ = ['RealNVPtemplate', 'PriorTemplate',
-           'Gaussian', 'MLP', 'RealNVP', 'CNN','FC'']
+           'Gaussian', 'MLP', 'RealNVP', 'CNN','FC']
