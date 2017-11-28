91f6a4b088467358a2df8a0b886f20d522fe16d5
Li SH <li012589li@outlook.com>
2017-11-28 16:23:23 +0800

    add test data at the end of buf.data

--- train/fit.py
+++ train/fit.py
@@ -10,31 +10,46 @@ from model import Gaussian,MLP,RealNVP
 from train import Ring2D, Ring5, Wave, Phi4, MCMC
 
 class Buffer(object):
-    def __init__(self,maximum,data=None):
+    def __init__(self,maximum,data=None,testRatio=0.3):
+        testSize = int(testRatio*maximum)
+        self.testRatio = testRatio
+        self.capacity = int(maximum*(1+self.testRatio))
         self.data = data
-        self.maximum = maximum
+        if data is None:
+            self.maximum = 0
+        else:
+            self.maximum = len(data)
     def draw(self,batchSize,testBatchSize=None):
-        if batchSize >self.data.shape[0]:
-            batchSize = self.data.shape[0]
-        perm = torch.randperm(self.data.shape[0])
+        maximum = int(self.maximum*(1.0-self.testRatio))
+        if batchSize > maximum:
+            batchSize = maximum
+        perm = torch.randperm(self.maximum)
         if testBatchSize is None:
             return self.data[perm[:batchSize]]
         else:
+            if testBatchSize > self.maximum-maximum:
+                testBatchSize = self.maximum-maximum
             train =self.data[perm[:batchSize]]
-            test = self.data[perm[batchSize:batchSize+testBatchSize]]
+            test = self.data[-testBatchSize:-1]
             return train,test
+    def drawtest(self,testBatchSize):
+        if testBatchSize>int(self.maximum*(self.testRatio)):
+            testBatchSize = int(self.maximum*(self.testRatio))
+        else:
+            print(testBatchSize)
+            test = self.data[-testBatchSize:-1]
+            return test
     def push(self,data):
         if self.data is None:
             self.data = data
         else:
             self.data = torch.cat([self.data,data],0)
-        if self.data.shape[0] > self.maximum:
+        if self.data.shape[0] > self.capacity:
             self._maintain()
-    def kill(self,ratio):
-        pass
+        self.maximum = len(self.data)
     def _maintain(self):
         perm = torch.randperm(self.data.shape[0])
-        self.data = self.data[perm[:self.maximum]]
+        self.data = self.data[perm[:self.capacity]]
 
 def train(model, Nepochs, supervised, buff, batchSize, modelname, lr = 5e-4,decay = 0.001,save = True, saveSteps=10, feed=True):
     LOSS=[]
@@ -49,14 +64,16 @@ def train(model, Nepochs, supervised, buff, batchSize, modelname, lr = 5e-4,deca
         if supervised:
             y_data = Variable(traindata[:, -1])
 
-        logp,z = model.logProbabilityWithInference(x_data)
+        logp = model.logProbability(x_data)
+        #logp,z = model.logProbabilityWithInference(x_data)
         if supervised:
             loss = criterion(logp, y_data)
         else:
             loss = -logp.mean()
 
         if feed:
-            print ("epoch:",epoch, "loss:",loss.data[0], "z mean:",np.mean(z.data.numpy()),"z std:",np.std(z.data.numpy()))
+            print ("epoch:",epoch, "loss:",loss.data[0])
+            #print ("epoch:",epoch, "loss:",loss.data[0], "z mean:",np.mean(z.data.numpy()),"z std:",np.std(z.data.numpy()))
         LOSS.append(loss.data[0])
 
         optimizer.zero_grad()
@@ -73,7 +90,7 @@ def train(model, Nepochs, supervised, buff, batchSize, modelname, lr = 5e-4,deca
 
 def test(model, supervised, buff, batchSize):
 
-    testdata = buff.draw(batchSize)
+    testdata = buff.drawtest(batchSize)
     x_data = Variable(testdata[:, 0:-1])
 
     if supervised:
@@ -86,6 +103,6 @@ def test(model, supervised, buff, batchSize):
     else:
         loss = -logp.mean()
 
-    print(torch.mean(loss))
+    print("test loss:",(loss.data)[0])
 
-    return loss
+    return loss.data
