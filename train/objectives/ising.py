558c18f6f1ed444fa00cb3329e59879539d3f7e9
Lei Wang <wangleiphy@gmail.com>
2017-12-03 14:44:50 +0800

    corrected bug

--- train/objectives/ising.py
+++ train/objectives/ising.py
@@ -9,12 +9,12 @@ class Ising(Target):
 
     def __init__(self):
         super(Ising, self).__init__(2,'Ising')
-        A = np.array([[0., -1.],
-                      [-1., 0.]], 
+        A = np.array([[0., 1.],
+                      [1., 0.]], 
                       dtype=np.float64)
     
         w, v = eigh(A)    
-        self.d = 1.0-w.min()
+        self.d = 1-w.min()
         self.Lambda = Variable(torch.from_numpy(w+self.d).view(-1,len(w)),  requires_grad=False)
         self.VT = Variable( torch.from_numpy(v.transpose()), requires_grad=False)
     
@@ -25,18 +25,19 @@ class Ising(Target):
 
     def energy(self, x): # actually logp
         return -0.5*(x**2/self.Lambda).sum(dim=1) \
-        + torch.log(torch.cosh(torch.mm(x, self.VT) -0.5*self.d)).sum(dim=1)
+        + torch.log(torch.cosh(torch.mm(x, self.VT))).sum(dim=1)
     
     def measure(self, x):
-        p = F.sigmoid(2.*torch.mm(x, self.VT) -self.d) 
-        s = torch.bernoulli(p).data.numpy()
+        p = torch.sigmoid(2.*torch.mm(Variable(x), self.VT)) 
+        s = 2*torch.bernoulli(p).data.numpy()-1
 
-        return 2*s-1 
+        return (s.sum(axis=1))**2
 
 if __name__=='__main__':
+    torch.manual_seed(42)
     ising = Ising() 
     x = Variable(torch.randn(10, 2).double())
     print (x)
     print (ising.energy(x))
-    print (ising.measure(x))
+    print (ising.measure(x.data))
 
