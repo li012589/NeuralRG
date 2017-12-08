92453cb30c2100034d301deec8be3350a0ac14d0
Lei Wang <wangleiphy@gmail.com>
2017-12-08 09:24:45 +0800

    minor

--- train/__init__.py
+++ train/__init__.py
@@ -1,4 +1,4 @@
-from .objectives import Ring2D, Ring5, Wave, Phi4, Mog2 
+from .objectives import Ring2D, Ring5, Wave, Phi4, Mog2, Ising
 from .metropolis import MCMC
 from .hmc import HMCSampler
 from .fit import train, test, Buffer
