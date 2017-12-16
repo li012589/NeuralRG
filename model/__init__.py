7143ab621dec01a338ce0654fda140126206562c
Lei Wang <wangleiphy@gmail.com>
2017-12-16 18:47:19 +0800

    resnet

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,8 @@
 from .template import RealNVPtemplate, PriorTemplate
 from .realnvp import Gaussian,Cauchy, GMM, RealNVP
 from .layers import MLP, CNN, FC, ScalableTanh,Squeezing
+from .resnet import ResNet
 
-__all__ = ['RealNVPtemplate', 'PriorTemplate','Gaussian', 'Cauchy', 'GMM', 'MLP', 'RealNVP', 'CNN','FC', 'ScalableTanh','Squeezing']
+__all__ = ['RealNVPtemplate', 'PriorTemplate','Gaussian', 'Cauchy', 'GMM', 'MLP', 'RealNVP', 'CNN','FC', 'ScalableTanh','Squeezing','ResNet']
 
 
---
+++ model/resnet.py
@@ -0,0 +1,91 @@
+import math 
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+# 3x3 Convolution
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                     stride=stride, padding=1, bias=True)
+
+# Residual Block
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+        
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    """
+    Resnet 
+
+    """
+
+    def __init__(self, channels, name="resnet", activation=F.tanh):
+        """
+
+        This mehtod initialise this class.
+        Args:
+            name (string): name of this class.
+        """
+        super(ResNet, self).__init__()
+        self.name = name
+        #self.layers = nn.ModuleList()
+        self.in_channels = 1
+        self.layer1 = self.make_layer(ResidualBlock, channels, 1)
+        self.layer2 = self.make_layer(ResidualBlock, 1, 1)
+        self.activation = activation
+        
+        #weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                #n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                #m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.fill_(0)
+
+    def make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = nn.Sequential(
+                conv3x3(self.in_channels, out_channels, stride=stride),
+                nn.BatchNorm2d(out_channels))
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for i in range(1, blocks):
+            layers.append(block(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        """
+
+        This mehtod calculate the nerual network output of input x.
+        Args:
+            x (torch.autograd.Variable): input variables.
+        Return:
+            x (torch.autograd.Variable): output variables.
+
+        """
+        x = self.layer1(x)
+        x = self.layer2(x)
+        return self.activation(x)
