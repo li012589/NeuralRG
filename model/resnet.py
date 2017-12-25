9eb43fe043321665a03355b208fda73c5c8c1874
Lei Wang <wangleiphy@gmail.com>
2017-12-25 08:43:11 +0800

    no bm in resnet

--- model/resnet.py
+++ model/resnet.py
@@ -14,19 +14,19 @@ class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(ResidualBlock, self).__init__()
         self.conv1 = conv3x3(in_channels, out_channels, stride)
-        self.bn1 = nn.BatchNorm2d(out_channels)
+        #self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = conv3x3(out_channels, out_channels)
-        self.bn2 = nn.BatchNorm2d(out_channels)
+        #self.bn2 = nn.BatchNorm2d(out_channels)
         self.downsample = downsample
         
     def forward(self, x):
         residual = x
         out = self.conv1(x)
-        out = self.bn1(out)
+        #out = self.bn1(out)
         out = self.relu(out)
         out = self.conv2(out)
-        out = self.bn2(out)
+        #out = self.bn2(out)
         if self.downsample:
             residual = self.downsample(x)
         out += residual
@@ -40,7 +40,7 @@ class ResNet(nn.Module):
 
     """
 
-    def __init__(self, channels, name="resnet", activation=F.tanh):
+    def __init__(self, channels, activation=F.tanh, name="resnet"):
         """
 
         This mehtod initialise this class.
@@ -52,7 +52,7 @@ class ResNet(nn.Module):
         #self.layers = nn.ModuleList()
         self.in_channels = channels 
         self.conv = conv3x3(1, channels)
-        self.bn = nn.BatchNorm2d(channels)
+        #self.bn = nn.BatchNorm2d(channels)
         self.relu = nn.ReLU(inplace=True)
         self.layer1 = self.make_layer(ResidualBlock, channels, 1)
         self.layer2 = self.make_layer(ResidualBlock, 1, 1)
@@ -91,7 +91,7 @@ class ResNet(nn.Module):
         """
 
         out = self.conv(x)
-        out = self.bn(out)
+        #out = self.bn(out)
         out = self.relu(out)
         out = self.layer1(out)
         out = self.layer2(out)
