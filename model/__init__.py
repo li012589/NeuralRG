6f51b27938bc9530bebb4093bc16ea004f1c762d
Li SH <li012589li@outlook.com>
2017-12-29 11:25:31 +0800

    move prior to dedicated file

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,8 @@
 from .template import RealNVPtemplate, PriorTemplate
-from .realnvp import Gaussian,Cauchy, GMM, RealNVP
+from .realnvp import RealNVP
 from .layers import MLP, CNN, FC, ScalableTanh,Squeezing
 from .resnet import ResNet
+from .prior import Gaussian,Cauchy, GMM
 
 __all__ = ['RealNVPtemplate', 'PriorTemplate','Gaussian', 'Cauchy', 'GMM', 'MLP', 'RealNVP', 'CNN','FC', 'ScalableTanh','Squeezing','ResNet']
 
