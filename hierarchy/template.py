e3f42c0d5990d15135c8d84b92f2b148cd3c81ce
Li SH <li012589li@outlook.com>
2018-01-03 17:39:21 +0800

    add __init__ and add api defines

--- hierarchy/template.py
+++ hierarchy/template.py
@@ -5,32 +5,62 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 #from .realnvp import RealNVP
-from .layer import Roll, Wide2bacth, Batch2wide
+from model import Roll, Wide2bacth, Batch2wide, Indentical
 
 
 class HierarchyBijector(nn.Module):
-    def __init__(self,filterSize,modelSize,layers,maskList,prior,name = None,double = False):
+    def __init__(self,layerSizeList,sizeList,rollList,bijectors,maskList,prior,name = None,double = False):
         # ONLY work for one dimension!!!!
-        super(TEBD,self).__init__()
+        super(HierarchyBijector,self).__init__()
+        assert len(layerSizeList) == len(sizeList)
+        assert len(sizeList) == len(bijectors)
+        assert len(bijectors) == len(maskList)
+        assert maskList.shape[0] == len(rollList)
+
         if double:
-            self.layers = torch.nn.ModuleList(layers).double()
+            self.bijectors = torch.nn.ModuleList(bijectors).double()
         else:
-            self.layers = torch.nn.ModuleList(layers).float()
-        Rolls = []
-        Reshapes = []
-        for i in range(len(layers)):
-            if i == 0:
-                Reshapes.append(Wide2bacth(filterSize))
-            elif i % 2 == 0:
-                Rolls.append(Roll(1,1))
+            self.bijectors = torch.nn.ModuleList(bijectors).float()
+
+        self.NumLayers = len(maskList)
+        self.maskList = maskList
+
+        RollLayers = []
+        ReshapeLayers = []
+        for i in range(self.NumLayers):
+            if i % 2 == 0:
+                ReshapesLayers.append(Wide2bacth(layerSizeList[i]))
             else:
-                Rolls.append(Roll(1,-1))
-        Reshapes.append(Batch2wide(modelSize))
-        self.Rolls = torch.nn.ModuleList(Rolls)
-        self.Reshapes = torch.nn.ModuleList(Reshapes)
-        assert maskList.shape[0] == len(layers)
+                ReshapesLayers.append(Batch2wide(layerSizeList[i]))
+            if i == 0:
+                RollLayers.append(Identical())
+            else:
+                RollLayers.append(Roll(rollList[i][0],rollList[i][1]))
+        self.RollLayers = torch.nn.ModuleList(RollLayers)
+        self.ReshapeLayers = torch.nn.ModuleList(ReshapeLayers)
         self.register_buffer(maskList)
 
     def inference(self,x,ifLogjac = False):
         for layer in layers:
-            x = 
+            pass
+
+    def generate(self,x,ifLogjac = False):
+        pass
+
+    def cuda(self,device = None, async = False):
+        pass
+
+    def cpu(self,device = None, async = False):
+        pass
+
+    def logProbability(self,x):
+        pass
+
+    def sample(self,batchSize):
+        pass
+
+    def saveModel(self, saveDict):
+        pass
+
+    def loadModel(self, saveDict):
+        pass
