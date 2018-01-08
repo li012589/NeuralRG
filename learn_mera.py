c79cb5292e75d77d5d7526ab0d4086092276c343
Lei Wang <wangleiphy@gmail.com>
2018-01-08 17:15:44 +0800

    learn_mera works for 2d

--- learn_mera.py
+++ learn_mera.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 from model import Gaussian, GMM, MLP,CNN,ResNet, RealNVP, ScalableTanh
 from train import Ring2D, Ring5, Wave, Phi4, Mog2, Ising
 from train import MCMC, Buffer
-from hierarchy import MERA 
+from hierarchy import MERA, MLPreshape 
 from learn_realnvp import learn_acc # FIXME 
 
 if __name__=="__main__":
@@ -44,6 +44,7 @@ if __name__=="__main__":
     group.add_argument("-modelname", default=None, help="load model")
     group.add_argument("-prior", default='gaussian', help="prior distribution")
     group.add_argument("-Nlayers", type=int, default=8, help="# of layers in RNVP block")
+    group.add_argument("-Ndisentangler", type=int, default=1, help="# of disentanglers")
     group.add_argument("-Hs", type=int, default=10, help="")
     group.add_argument("-Ht", type=int, default=10, help="")
     group.add_argument("-train_model", action='store_true', help="actually train model")
@@ -75,7 +76,6 @@ if __name__=="__main__":
     else:
         print ('use float32')
 
-    netDimension = 2
     if args.target == 'ring2d':
         target = Ring2D()
     elif args.target == 'ring5':
@@ -88,7 +88,6 @@ if __name__=="__main__":
         target = Phi4(4,2,0.15,1.145)
     elif args.target == 'ising':
         target = Ising(args.L, args.d, args.T, cuda, args.double)
-        netDimension = args.d+1
     else:
         print ('what target ?', args.target)
         sys.exit(1)
@@ -96,8 +95,8 @@ if __name__=="__main__":
     Nvars = target.nvars 
 
     if args.prior == 'gaussian':
-        if netDimension == 3:
-            prior = Gaussian([1, args.L, args.L], requires_grad = args.train_prior)
+        if args.d== 2:
+            prior = Gaussian([args.L, args.L], requires_grad = args.train_prior)
         else:
             prior = Gaussian([Nvars], requires_grad = args.train_prior)
     elif args.prior == 'gmm':
@@ -132,22 +131,23 @@ if __name__=="__main__":
     subprocess.check_call(cmd)
     
     #RNVP block
-    depth = 2*int(math.log(Nvars,2))
+    kernel_size = [2]*args.d 
+    mlpsize = int(np.product(np.array(kernel_size)))
+    depth = int(math.log(Nvars,mlpsize))*(args.Ndisentangler +1)
+
     print ('depth of the mera network', depth)
-    sList = [[MLP(2, args.Hs, activation=ScalableTanh([2])) for _ in range(args.Nlayers)] for l in range(depth)]
-    tList = [[MLP(2, args.Ht) for _ in range(args.Nlayers)] for l in range(depth)]
+    sList = [[MLPreshape(mlpsize, args.Hs, activation=ScalableTanh([mlpsize])) for _ in range(args.Nlayers)] for l in range(depth)]
+    tList = [[MLPreshape(mlpsize, args.Ht) for _ in range(args.Nlayers)] for l in range(depth)]
     masktypelist = ['channel', 'channel'] * (args.Nlayers//2)
     
     #assamble RNVP blocks into a MERA
-    input_size= [Nvars]
-    prior = Gaussian([Nvars])
-    layers = [RealNVP([2], 
+    layers = [RealNVP(kernel_size, 
                       sList[l], 
                       tList[l], 
-                      Gaussian([2]), 
+                      None, 
                       masktypelist) for l in range(depth)] 
     
-    model = MERA(1, 2, Nvars, layers, prior, name=key)
+    model = MERA(args.d, kernel_size, Nvars, layers, prior, metaDepth =args.Ndisentangler+1, name=key)
 
     if args.modelname is not None:
         try:
@@ -163,7 +163,7 @@ if __name__=="__main__":
     if args.train_model:
         print("train model", key)
         model, LOSS = learn_acc(target, model, args.Nepochs,args.Batchsize, 
-                                args.Ntherm, args.Nsteps, args.Nskips,input_size,
+                                args.Ntherm, args.Nsteps, args.Nskips,
                                 epsilon=args.epsilon,beta=args.beta, 
                                 delta=args.delta, omega=args.omega, lr=args.lr, 
                                 cuda = cuda, exact=args.exact)
