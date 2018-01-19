46f668217b2109baea7e2e323a3918f4186aebcc
Lei Wang <wangleiphy@gmail.com>
2018-01-19 15:36:47 +0800

    semilogx zoomed in view

--- paper/load_mcresults.py
+++ paper/load_mcresults.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from config import * 
 from matplotlib.offsetbox import AnchoredText
 
-def loss_and_acc(h5):
+def loss_and_acc(h5, exact=None):
 
     loss = np.array(h5['results']['loss'])
 
@@ -14,11 +14,31 @@ def loss_and_acc(h5):
     ax1.get_xaxis().set_visible(False)
     at = AnchoredText("(a)",prop=dict(size=18), frameon=False,loc=2,)
     plt.gca().add_artist(at)
-
+    if exact is not None:
+        plt.gca().axhline(exact, color='r', lw=2)
+    at = AnchoredText("(b)",prop=dict(size=18), frameon=False,loc=2,)
     plt.ylabel('$\mathcal{L}$')
+
+
+    ###################
+    from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes, mark_inset
+    #axins = zoomed_inset_axes(ax1, 1.5,  loc=7)
+    axins = plt.axes([0.45, 0.65, 0.4, 0.2])
+    axins.semilogx(loss[:, 1], lw=2)
+    if exact is not None:
+        plt.gca().axhline(exact, color='r', lw=2)
+    
+    x1, x2 = 100, 2000
+    y1, y2 = -150, -140
+    axins.set_xlim(x1, x2)
+    axins.set_ylim(y1, y2)
+    #axins.xaxis.set_visible(False)
+    #axins.yaxis.set_visible(False)
+    mark_inset(ax1, axins, loc1=1, loc2=2, fc='none', ec='0.5')
+    ###################
+
     plt.subplot(212, sharex=ax1)
     plt.plot(loss[:, 2], lw=2)
-    at = AnchoredText("(b)",prop=dict(size=18), frameon=False,loc=2,)
     plt.gca().add_artist(at)
 
     plt.ylabel('acceptance rate')
@@ -33,6 +53,7 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument("-filename", help="filename")
     parser.add_argument("-index", type=int, default=1, help="figure index")
+    parser.add_argument("-exact", type=float, default=None, help="exact lower bound")
 
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-show", action='store_true',  help="show figure right now")
@@ -43,7 +64,7 @@ if __name__=='__main__':
     
     #TODO: better selection 
     if args.index==1:
-        loss_and_acc(h5)
+        loss_and_acc(h5, args.exact)
     elif args.index==2:
         pass
     elif args.index==3:
