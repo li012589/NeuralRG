db6624595844f4afde5368c5249e0ff59049535a
Li SH <li012589li@outlook.com>
2018-03-08 17:21:46 +0800

    remove hmc

--- train/__init__.py
+++ train/__init__.py
@@ -1,6 +1,5 @@
 from .objectives import Ring2D, Ring5, Wave, Phi4, Mog2, Ising
 from .metropolis import MCMC
-from .hmc import HMCSampler
 from .fit import train, test, Buffer
 
-__all__ = ['Ring2D', 'Ring5', 'Wave', 'Phi4', 'Mog2', 'Ising', 'MCMC', 'HMCSampler', 'train', 'test', 'Buffer']
+__all__ = ['Ring2D', 'Ring5', 'Wave', 'Phi4', 'Mog2', 'Ising', 'MCMC', 'train', 'test', 'Buffer']
