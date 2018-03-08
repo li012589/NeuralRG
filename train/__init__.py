0277ba977951970ab8dc4e59dc575578f3962dd9
Li SH <li012589li@outlook.com>
2018-03-08 16:44:26 +0800

    add all

--- train/__init__.py
+++ train/__init__.py
@@ -2,3 +2,5 @@ from .objectives import Ring2D, Ring5, Wave, Phi4, Mog2, Ising
 from .metropolis import MCMC
 from .hmc import HMCSampler
 from .fit import train, test, Buffer
+
+__all__ = ['Ring2D', 'Ring5', 'Wave', 'Phi4', 'Mog2', 'Ising', 'MCMC', 'HMCSampler', 'train', 'test', 'Buffer']
