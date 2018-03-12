0403aca01b640c3a860223114c10b303abf0a3d9
Li SH <li012589li@outlook.com>
2018-03-12 15:11:44 +0800

    remove utils

--- utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .dataLoader import dataSet
-from .timer import Timer
-from .autoCorrelation import autoCorrelationTimewithErr
-from .acceptRate import acceptanceRate
-from .dataCheck import checkNan
-
-#__all__ = ["torchGPUmanager","dataSet", 'Timer']
--- utils/metropolis.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import torch
-from torch.autograd import Variable
-
-def metropolis(e1,e2):
-    diff = e1-e2
-    return diff.exp()-diff.uniform_()>=0.0
