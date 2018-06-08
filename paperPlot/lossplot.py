670fd9826e8241b8b08d2d560ce48c571a41e4cc
Li SH <li012589li@outlook.com>
2018-06-08 17:11:46 +0800

    udpate plot script for paper

--- paperPlot/lossplot.py
+++ paperPlot/lossplot.py
@@ -31,33 +31,35 @@ def loss_and_acc(h5,L,savePeriod,Lexact=None,Hexact=None):
 
     plt.figure(figsize=(8,6))
     ax1 = plt.subplot(211)
-    plt.plot(loss/L, lw=2)
-    ax1.get_xaxis().set_visible(False)
+    plt.semilogx(loss/L, lw=2)
+    #ax1.get_xaxis().set_visible(False)
     at = AnchoredText("(a)",prop=dict(size=18), frameon=False,loc=2,)
     plt.gca().add_artist(at)
     at = AnchoredText("(b)",prop=dict(size=18), frameon=False,loc=2,)
     plt.ylabel('$\mathcal{L}/N$')
+    plt.xlim([0,5000])
+    plt.xlabel('iterations')
 
 
     ###################
-    from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes, mark_inset
+    #from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes, mark_inset
     #axins = zoomed_inset_axes(ax1, 1.5,  loc=7)
-    axins = plt.axes([0.45, 0.65, 0.4, 0.2])
-    axins.semilogx(loss/L, lw=2)
+    #axins = plt.axes([0.45, 0.65, 0.4, 0.2])
+    #axins.semilogx(loss/L, lw=2)
 
     if Lexact is not None:
         plt.gca().axhline(Lexact, color='r', lw=2)
 
-    x1, x2 = 50, 4900
-    y1, y2 = -2.35, -2.25
-    axins.set_xlim(x1, x2)
-    axins.set_ylim(y1, y2)
+    #x1, x2 = 50, 4900
+    #y1, y2 = -2.35, -2.25
+    #axins.set_xlim(x1, x2)
+    #axins.set_ylim(y1, y2)
     #axins.xaxis.set_visible(False)
     #axins.yaxis.set_visible(False)
-    mark_inset(ax1, axins, loc1=1, loc2=2, fc='none', ec='0.5')
+    #mark_inset(ax1, axins, loc1=1, loc2=2, fc='none', ec='0.5')
     ###################
 
-    plt.subplot(212, sharex=ax1)
+    plt.subplot(212)
 #    plt.plot(loss[:, 2], lw=2)
     plt.gca().add_artist(at)
 
@@ -71,6 +73,7 @@ def loss_and_acc(h5,L,savePeriod,Lexact=None,Hexact=None):
     plt.subplots_adjust(hspace=0.1, bottom=0.15,left=0.15)
     plt.ylim([0,0.72])
     plt.xlim([-50,5000])
+    plt.tight_layout()
 
 
 if __name__=='__main__':
