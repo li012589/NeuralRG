e88c554fac137c54a1f306627da85bb06c26ed16
Li SH <li012589li@outlook.com>
2018-06-26 23:20:44 +0800

    add tests for mera

--- test/flowRelated.py
+++ test/flowRelated.py
@@ -2,16 +2,16 @@ import torch
 import numpy as np
 from numpy.testing import assert_array_almost_equal,assert_array_equal
 
-def bijective(flow,batch=100):
+def bijective(flow,batch=100,decimal=5):
     x,p = flow.sample(batch)
     z,ip = flow.inference(x)
     xz,gp = flow.generate(z)
     op = flow.prior.logProbability(z)
-    assert_array_almost_equal(x.detach().numpy(),xz.detach().numpy())
-    assert_array_almost_equal(ip.detach().numpy(),-gp.detach().numpy())
-    assert_array_almost_equal(p.detach().numpy(),(op+gp).detach().numpy())
+    assert_array_almost_equal(x.detach().numpy(),xz.detach().numpy(),decimal=decimal)
+    assert_array_almost_equal(ip.detach().numpy(),-gp.detach().numpy(),decimal=decimal)
+    assert_array_almost_equal(p.detach().numpy(),(op+gp).detach().numpy(),decimal=decimal)
 
-def saveload(flow,blankFlow,batch=100):
+def saveload(flow,blankFlow,batch=100,decimal=5):
     x,p = flow.sample(batch)
     z,ip = flow.inference(x)
     d = flow.save()
@@ -20,6 +20,6 @@ def saveload(flow,blankFlow,batch=100):
     blankFlow.load(dd)
     op = blankFlow.prior.logProbability(z)
     xz,gp = blankFlow.generate(z)
-    assert_array_almost_equal(x.detach().numpy(),xz.detach().numpy())
-    assert_array_almost_equal(ip.detach().numpy(),-gp.detach().numpy())
-    assert_array_almost_equal(p.detach().numpy(),(op+gp).detach().numpy())
+    assert_array_almost_equal(x.detach().numpy(),xz.detach().numpy(),decimal=decimal)
+    assert_array_almost_equal(ip.detach().numpy(),-gp.detach().numpy(),decimal=decimal)
+    assert_array_almost_equal(p.detach().numpy(),(op+gp).detach().numpy(),decimal=decimal)
---
+++ test/test_model_mera.py
@@ -0,0 +1,91 @@
+from flowRelated import *
+
+import os
+import sys
+sys.path.append(os.getcwd())
+
+import torch
+from torch import nn
+import numpy as np
+import utils
+import flow
+import source
+
+def test_bijective():
+    p = source.Gaussian([4,4])
+
+    BigList = []
+    for _ in range(2*2*2):
+        maskList = []
+        for n in range(4):
+            if n %2==0:
+                b = torch.zeros(1,4)
+                i = torch.randperm(b.numel()).narrow(0, 0, b.numel() // 2)
+                b.zero_()[:,i] = 1
+                b=b.view(1,2,2)
+            else:
+                b = 1-b
+            maskList.append(b)
+        maskList = torch.cat(maskList,0).to(torch.float32)
+        BigList.append(maskList)
+
+    layers = [flow.RNVP(BigList[n], [utils.SimpleMLPreshape([4,32,32,4],[nn.ELU(),nn.ELU(),None]) for _ in range(4)], [utils.SimpleMLPreshape([4,32,32,4],[nn.ELU(),nn.ELU(),utils.ScalableTanh(4)]) for _ in range(4)]
+) for n in range(2*2*2)]
+
+    length = 4
+    repeat = 2
+
+    t = flow.MERA(2,length,layers,repeat,p)
+    bijective(t)
+
+def test_saveload():
+    p = source.Gaussian([4,4])
+
+    BigList = []
+    for _ in range(2*2*2):
+        maskList = []
+        for n in range(4):
+            if n %2==0:
+                b = torch.zeros(1,4)
+                i = torch.randperm(b.numel()).narrow(0, 0, b.numel() // 2)
+                b.zero_()[:,i] = 1
+                b=b.view(1,2,2)
+            else:
+                b = 1-b
+            maskList.append(b)
+        maskList = torch.cat(maskList,0).to(torch.float32)
+        BigList.append(maskList)
+
+    layers = [flow.RNVP(BigList[n], [utils.SimpleMLPreshape([4,32,32,4],[nn.ELU(),nn.ELU(),None]) for _ in range(4)], [utils.SimpleMLPreshape([4,32,32,4],[nn.ELU(),nn.ELU(),utils.ScalableTanh(4)]) for _ in range(4)]
+) for n in range(2*2*2)]
+
+    length = 4
+    repeat = 2
+
+    t = flow.MERA(2,length,layers,repeat,p)
+
+    p = source.Gaussian([4,4])
+
+    BigList = []
+    for _ in range(2*2*2):
+        maskList = []
+        for n in range(4):
+            if n %2==0:
+                b = torch.zeros(1,4)
+                i = torch.randperm(b.numel()).narrow(0, 0, b.numel() // 2)
+                b.zero_()[:,i] = 1
+                b=b.view(1,2,2)
+            else:
+                b = 1-b
+            maskList.append(b)
+        maskList = torch.cat(maskList,0).to(torch.float32)
+        BigList.append(maskList)
+
+    layers = [flow.RNVP(BigList[n], [utils.SimpleMLPreshape([4,32,32,4],[nn.ELU(),nn.ELU(),None]) for _ in range(4)], [utils.SimpleMLPreshape([4,32,32,4],[nn.ELU(),nn.ELU(),utils.ScalableTanh(4)]) for _ in range(4)]
+) for n in range(2*2*2)]
+
+    length = 4
+    repeat = 2
+
+    blankt = flow.MERA(2,length,layers,repeat,p)
+    saveload(t,blankt)
