3ebc9383a38a011229f01c053bd483f8b10a7d88
Li SH <li012589li@outlook.com>
2018-09-04 13:49:25 +0800

    add Kij form of phi4comlex

--- source/__init__.py
+++ source/__init__.py
@@ -1,6 +1,6 @@
 from .gaussian import Gaussian
 from .phi4 import Phi4
-from .phi4complex import Phi4c
+from .phi4complex import Phi4c,Phi4complex
 from .ringLike import Ring2d
 from .ising import Ising
 from .source import Source
--- source/phi4complex.py
+++ source/phi4complex.py
@@ -4,6 +4,43 @@ import torch
 from .source import Source
 from utils import roll
 
+def no2ij(n,dList):
+    cood =  []
+    d = len(dList)
+    for dim in reversed(range(len(dList))):
+        L = dList[dim]
+        tmp1 = (int(n/(L**dim)))
+        n -= tmp1*(L**dim)
+        cood.append(tmp1)
+    return cood
+
+def ij2no(cood,dList):
+    n = 0
+    d = len(dList)
+    for dim in reversed(range(len(dList))):
+        L = dList[dim]
+        n += (cood[d-dim-1])*(L**dim)
+    return n
+
+def Kijbuilder(dList,k,lamb,peridic=True,skip=None):
+    maxNo = 1
+    for d in dList:
+        maxNo *= d
+    Kij = torch.zeros([maxNo]*2)
+    for no in range(maxNo):
+        cood = no2ij(no,dList)
+        for i in range(len(cood)):
+            if i in skip:
+                continue
+            coodp = cood.copy()
+            coodp[i] = (cood[i]+1)%dList[i]
+            Kij[no,ij2no(coodp,dList)] = k
+            coodp[i] = (cood[i]-1)%dList[i]
+            Kij[no,ij2no(coodp,dList)] = k
+    tmp = torch.diag(torch.tensor([lamb]*(maxNo),dtype=torch.float32))
+    return Kij+tmp
+
+
 class Phi4c(Source):
     def __init__(self,l,dims,kappa,lamb,name = None):
         if name is None:
@@ -37,4 +74,30 @@ class Phi4c(Source):
         S *= -2*self.kappa
         term2 *= self.lamb
         out = S[:,0]-S[:,1]+term2+term1
+        return out
+
+
+class Phi4complex(Source):
+    def __init__(self,l,dims,kappa,lamb,name = None):
+        if name is None:
+            self.name = "phi4_l"+str(l)+"_d"+str(dims)+"_kappa"+str(kappa)+"_lamb"+str(lamb)
+        else:
+            self.name = name
+
+        nvars = [2]
+        for _ in range(dims):
+            nvars += [l]
+        super(Phi4c,self).__init__(nvars,name)
+
+        self.K = Kijbuilder([2]+[l]*dims,kappa,0,skip=0)
+        maxNo = self.K.shape[0]
+        diag = torch.diagonal(self.K)
+        self.K[int(maxNo/2):,int(maxNo/2):] = -self.K[int(maxNo/2):,int(maxNo/2)]
+        self.K += torch.diag(torch.tensor([1]*(maxNo),dtype=torch.float32))
+
+    def sample(self, batchSize, thermalSteps = 50, interSteps=5, epsilon=0.1):
+        return self._sampleWithHMC(batchSize,thermalSteps,interSteps, epsilon)
+
+    def energy(self,x):
+        out = torch.mm(torch.mm(x.reshape(1,-1),self.K),x.reshape(-1,1))
         return out
