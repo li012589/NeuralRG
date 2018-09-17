163cf075bfc586c7a2edb8c5f474a62d3c00c585
Li SH <li012589li@outlook.com>
2018-09-17 17:39:14 +0800

    add integration

---
+++ inta.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+import numpy as np
+
+import utils
+import flow
+import train
+import source
+
+import argparse
+
+parser = argparse.ArgumentParser(description='')
+
+group = parser.add_argument_group('Ising target parameters')
+#
+group.add_argument("-L",type=int, default=2,help="linear size")
+group.add_argument("-d",type=int, default=2,help="dimension")
+#group.add_argument("-T",type=float, default=2.269185314213022, help="Temperature")
+group.add_argument("-kappa",type=float, default=0.15, help="Kappa")
+group.add_argument("-lamb",type=float, default=1.145, help="Lambda")
+
+group = parser.add_argument_group('integration parameters')
+#
+group.add_argument("-start",type=int, default=-2,help="start point")
+group.add_argument("-end",type=int, default=2,help="end point")
+group.add_argument("-points",type=int, default=10,help="sampled points at each dimension")
+
+args = parser.parse_args()
+
+def expandSpace(start,end,dims,num,endpoint=True):
+    space = []
+    for _ in range(dims):
+        space.append(np.linspace(start,end,num,endpoint=endpoint))
+    space = np.meshgrid(*space)
+    space = np.concatenate(space,axis=0)
+    space = space.reshape(dims,num**dims).transpose([1,0])
+    return torch.from_numpy(space)#.to(torch.float32)
+
+with torch.no_grad():
+    t = source.Phi4(args.L,args.d,args.kappa,args.lamb).to(torch.float64)
+    volume = (args.end-args.start)**(args.L**args.d)
+    enfn = lambda space,volume: torch.exp(-t.energy(space)).mean()*volume
+    space = expandSpace(args.start,args.end,args.L**args.d,args.points)
+    En = enfn(space,volume)
+    print(En.item())
+    print(-torch.log(En).item())
