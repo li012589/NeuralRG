f8c3e1b7779f92cb9f059249892e4a7d2e91c8e5
Li SH <li012589li@outlook.com>
2018-09-22 20:52:14 +0800

    change functional.tanh to torch.tanh

--- utils/layers/scalableTanh.py
+++ utils/layers/scalableTanh.py
@@ -1,12 +1,11 @@
 import math
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 class ScalableTanh(nn.Module):
     def __init__(self,input_size):
         super(ScalableTanh,self).__init__()
         self.scale = nn.Parameter(torch.zeros(input_size),requires_grad=True)
     def forward(self,x):
-        return self.scale * F.tanh(x)
+        return self.scale * torch.tanh(x)
 
