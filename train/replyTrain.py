fd5636321877b5fb4f338de0007260c23de671b2
Li SH <li012589li@outlook.com>
2018-09-29 17:15:12 +0800

    fit changed api

--- train/replyTrain.py
+++ train/replyTrain.py
@@ -42,7 +42,7 @@ def replySymmetryMERAInit(L,d,nlayers,nmlp,nhidden,nrepeat,symmetryList,device,d
 
     layers = [flow.RNVP(MaskList[n], [utils.SimpleMLPreshape(dimList,[nn.ELU() for _ in range(nmlp)]+[None]) for _ in range(nlayers)], [utils.SimpleMLPreshape(dimList,[nn.ELU() for _ in range(nmlp)]+[utils.ScalableTanh(coreSize)]) for _ in range(nlayers)]) for n in range(depth)]
 
-    f = flow.MERA(2,L,layers,nrepeat,s)
+    f = flow.MERA(2,L,layers,nrepeat,prior = s)
     f = Symmetrized(f,symmetryList,name = name)
     f.to(device = device,dtype = dtype)
     return f
