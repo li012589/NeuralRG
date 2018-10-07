727d9ebb7c517827cc6a38af19d00a0739cc8e84
Li Shuo-hui <contact_lish@iphy.ac.cn>
2018-10-07 16:59:01 +0800

    change setting

--- setting.py
+++ setting.py
@@ -3,13 +3,13 @@ import re
 import numpy as np
 import h5py
 
-maximumJobs = 7
+maximumJobs = 8
 
-command = ['python','./replyMain.py','-epochs','50','-batch','512','-nlayers','10','-nmlp','3','-nhidden','10','-L','32','-nrepeat','1','-savePeriod','100','-alpha','1']
+command = ['python','./replyMain.py','-epochs','5000','-batch','512','-nlayers','10','-nmlp','3','-nhidden','10','-L','32','-nrepeat','1','-savePeriod','100','-alpha','1']
 
-settings = [['-cuda',str(i)] for i in range(7)]
+settings = [['-cuda',str(i)] for i in range(8)]
 
-parameters = {"-T":[str(i/10) for i in range(20,36)],"-depthMERA":[str(3),str(5)]}
+parameters = {"-T":[str(i/10) for i in range(20,36)],"-depthMERA":[i+1 for i in range(5)]}
 
 def before():
     #print("this is pre-process")
@@ -20,22 +20,30 @@ def after():
     pass
 
 def finish(result):
-    res = []
+    loss = []
+    std = []
     for j in parameters['-depthMERA']:
-        tmp = []
+        tmploss = []
+        tmpstd = []
         for i in parameters['-T']:
-            tmp.append(result['-T '+str(i),+' -depthMERA ' +str(j)])
-        res.append(tmp)
-    print(res)
-    res = np.array(res)
+            tmploss.append(result['-T '+i+' -depthMERA ' +j+' '][-1][-2])
+            tmpstd.append(result['-T '+i+' -depthMERA ' +j+' '][-1][-1])
+        loss.append(tmploss)
+        std.append(tmpstd)
+    print('loss:',loss)
+    print('std:',std)
+
+    loss = np.array(loss)
+    std = np.array(std)
     with h5py.File("./core_result.hdf5","w") as f:
-        f.create_dataset('result',res)
+        f.create_dataset('loss',data = loss)
+        f.create_dataset('std',data = std)
 
 def process(result):
     nums = []
     for i in result[-2:-1]:
         nums.append([float(s) for s in re.findall(r'-?\d+\.?\d*',i)])
-    return np.array(nums)
+    return nums
 
 if settings != []:
     assert len(settings) == maximumJobs
