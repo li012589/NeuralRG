09fc18664de257967de29952639d48db533aa647
Li SH <li012589li@outlook.com>
2018-10-10 09:58:30 +0800

    change settings

--- setting.py
+++ setting.py
@@ -7,7 +7,7 @@ maximumJobs = 8
 
 command = ['python','./replyMain.py','-epochs','5000','-batch','512','-nlayers','10','-nmlp','10','-nhidden','10','-L','32','-nrepeat','1','-savePeriod','100','-alpha','1']
 
-settings = [['-cuda',str(i+1)] for i in range(8)]
+settings = [['-cuda',str(i)] for i in range(8)]
 
 parameters = {"-T":[str(i/10) for i in range(20,28)],"-depthMERA":[str(i+1) for i in range(4)][::-1]}
 
