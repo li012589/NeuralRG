956ff38798099e5296e68ad12346fe760fd6a69d
Li SH <li012589li@outlook.com>
2018-10-11 11:09:19 +0800

    change settings

--- setting.py
+++ setting.py
@@ -5,11 +5,11 @@ import h5py
 
 maximumJobs = 8
 
-command = ['python','./replyMain.py','-epochs','5000','-batch','512','-nlayers','10','-nmlp','10','-nhidden','10','-L','32','-nrepeat','1','-savePeriod','100','-alpha','1']
+command = ['python','./replyMain.py','-epochs','5000','-batch','512','-nlayers','10','-nmlp','12','-nhidden','10','-L','16','-nrepeat','1','-savePeriod','100','-alpha','1']
 
 settings = [['-cuda',str(i)] for i in range(8)]
 
-parameters = {"-T":[str(i/10) for i in range(20,28)],"-depthMERA":[str(i+1) for i in range(5)][::-1]}
+parameters = {"-T":[str(i/10) for i in range(20,33)],"-depthMERA":[str(i+1) for i in range(4)][::-1]}
 
 def before():
     #print("this is pre-process")
